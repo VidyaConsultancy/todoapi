2252e74c14d8fa1ae0ea90a4a1d538f425df3861
Arpit <arpit_jain@live.com>
2022-05-25 16:19:21 +0530

    feat: user detail and todos list api endpoints

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const PORT = 3000;
 const cacheMemory = {};
 
 app.get("/", (req, res, next) => {
+  console.log(req.headers);
   return res.send("Welcome to express server");
 });
 
 app.get("/users", (req, res, next) => {
+  console.log(req.params, req.query);
   const users = cacheMemory["users"];
   if (users) {
     return res.json({
@@ -48,6 +50,103 @@ app.get("/users", (req, res, next) => {
       message: "User list retrieved",
       error: null,
       data: { users },
+      resource: req.url,
+    });
+  });
+});
+
+app.get("/users/:userId", (req, res, next) => {
+  const { userId } = req.params;
+  fs.readFile("./db.json", (err, data) => {
+    if (err) {
+      // res.statusCode = 500;
+      // res.statusMessage = err.message;
+      return res.status(500).json({
+        success: false,
+        code: 500,
+        message: err.message,
+        error: err,
+        data: null,
+        resource: req.url,
+      });
+    }
+
+    const dataString = data.toString();
+    const dbObject = JSON.parse(dataString);
+    const users = dbObject.users;
+    const user = users.filter((user) => user.id === +userId);
+
+    if (Array.isArray(user) && user.length) {
+      return res.json({
+        success: true,
+        code: 200,
+        message: "User details",
+        error: null,
+        data: { user: user[0] },
+        resource: req.url,
+      });
+    }
+
+    return res.status(404).json({
+      success: false,
+      code: 404,
+      message: "User details not found",
+      error: null,
+      data: null,
+      resource: req.url,
+    });
+  });
+});
+
+app.get("/todos", (req, res, next) => {
+  const { userId } = req.query;
+  fs.readFile("./db.json", (err, data) => {
+    if (err) {
+      // res.statusCode = 500;
+      // res.statusMessage = err.message;
+      return res.status(500).json({
+        success: false,
+        code: 500,
+        message: err.message,
+        error: err,
+        data: null,
+        resource: req.url,
+      });
+    }
+
+    const dataString = data.toString();
+    const dbObject = JSON.parse(dataString);
+    const todos = dbObject.todos;
+    if (userId) {
+      const userTodos = todos.filter((todo) => todo.userId === +userId);
+
+      if (Array.isArray(userTodos) && userTodos.length) {
+        return res.json({
+          success: true,
+          code: 200,
+          message: "User todos list",
+          error: null,
+          data: { todos: userTodos },
+          resource: req.url,
+        });
+      }
+
+      return res.status(404).json({
+        success: false,
+        code: 404,
+        message: "User todos not available",
+        error: null,
+        data: null,
+        resource: req.url,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      code: 200,
+      message: "Todos list",
+      error: null,
+      data: { todos },
       resource: req.url,
     });
   });
