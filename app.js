6aff02001267642e492ff6738cc98d4d34dfa650
Arpit <arpit_jain@live.com>
2022-06-17 12:13:05 +0530

    updates CORS policy

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 const apiRouter = require("./routes/apis/index");
 const webRouter = require("./routes/web");
@@ -18,7 +18,7 @@ const helmet = require("helmet");
 app.use(logger("dev"));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors());
 app.use(helmet.hidePoweredBy());
 app.use(helmet.crossOriginResourcePolicy())
 
